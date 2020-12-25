6c4b5a3f3b29cbfd79aff9d291475c5e4562b8c7
morelmiles <morelmiles@gmail.com>
2020-12-25 16:41:23 +0300

    Add the hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,19 +3,21 @@ import bg from "../assets/img/bg.png";
 
 const Hero = () => {
   return (
-    <header className="masthead img-responsive">
-      <div className="container h-100">
-        <div className="row h-100 align-items-center">
-          <div className="col-12  text-center ">
-            <h1 className=" header-text ">
-              Visualizing the creation of career growth opportunities for every
-              member of the community.
-            </h1>
+       <header class="masthead">
+      <div class="intro-body">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <h1 class="brand-heading">Enabling Enterprise in Uganda</h1>
+<p class="intro-text">Call for applications- <a href="https://inplaceofwar.net/">In Place of War</a>, in partnership with the Mastercard Foundation, is launching an innovative certified training programme, mentoring network and awards to support the development of creative and social enterprise in Uganda.</p>
+              <a href="#about" class="btn btn-circle js-scroll-trigger">
+                <i class="fa fa-angle-double-down animated"></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </header>
-  );
+    </header>  );
 };
 
 export default Hero;
